584189017e9d7c23848de4be00e9f02f5f15d6a0
kycodee <ky.patton87@gmail.com>
2025-01-03 08:29:09 -0600

    Testing components with onClick to get idea of conditional render

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import YearMakeModelBar from './YearMakeModelBar';
 import AISearchBar from './AISearchBar';
@@ -7,15 +8,46 @@ import RepairInstructionsBox from './RepairInstructions';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App" >
-      <h1>AutoPit AI</h1>
-      <Container fluid className="d-flex flex-column justify-content-center  align-items-center ">
+
+  const [toolsNeeded, setToolsNeeded] = useState<string[]>([])
+
+  function showToolsAndInstructions() {
+    if(toolsNeeded.length !== 0){
+      return (
+        <Container fluid className="d-flex flex-column justify-content-center  align-items-center ">
         <YearMakeModelBar />
         <AISearchBar />
         <ToolsNeededBox />
         <RepairInstructionsBox />
       </Container>
+      )
+    } else {
+      return (
+        <Container fluid className="d-flex flex-column justify-content-center  align-items-center ">
+        <YearMakeModelBar />
+        <AISearchBar />
+      </Container>
+      )
+    }
+  }
+
+  useEffect(() => {
+
+  }, [toolsNeeded])
+
+  return (
+    <div className="App" >
+      <h1 onClick={() => {
+        const tools = ['socket', 'wrench', 'screwdriver']
+        setToolsNeeded(tools)
+      }}>AutoPit AI</h1>
+      {/* <Container fluid className="d-flex flex-column justify-content-center  align-items-center ">
+        <YearMakeModelBar />
+        <AISearchBar />
+        <ToolsNeededBox />
+        <RepairInstructionsBox />
+      </Container> */}
+      {showToolsAndInstructions()}
     </div>
   );
 }
