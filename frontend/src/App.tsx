0d2423e2ff5c004ad44f5d11d44d5ec12c96395b
kycodee <ky.patton87@gmail.com>
2025-01-22 17:00:09 -0600

    Added background image for landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import YearMakeModelBar from './YearMakeModelBar';
 import AISearchBar from './AISearchBar';
 import ToolsNeededBox from './ToolsNeeded';
 import LandingPage from './landing';
 import RepairInstructionsBox from './RepairInstructions';
 import { YearsListProps, Year } from './types_YearMakeModel';
+import toolImage from './pexels-dmitry-demidov-515774-6789021.jpg'
 import './App.css';
 
 function App() {
@@ -44,7 +45,7 @@ function App() {
           {/* <YearMakeModelBar years={modelYears}/>
           <AISearchBar /> */}
           <YearMakeModelBar years={modelYears} changeCarName={setCurrentCarName} />
-          <AISearchBar currentCarName={currentCarName}/>
+          <AISearchBar currentCarName={currentCarName} />
           <ToolsNeededBox />
           <RepairInstructionsBox />
         </Container>
@@ -52,8 +53,8 @@ function App() {
     } else {
       return (
         <Container fluid className="d-flex flex-column justify-content-center  align-items-center ">
-          <YearMakeModelBar years={modelYears} changeCarName={setCurrentCarName}/>
-          <AISearchBar currentCarName={currentCarName}/>
+          <YearMakeModelBar years={modelYears} changeCarName={setCurrentCarName} />
+          <AISearchBar currentCarName={currentCarName} />
         </Container>
       )
     }
@@ -69,7 +70,15 @@ function App() {
   }, [toolsNeeded])
 
   return (
-    <div className="App" >
+    <div className="App"  >
+    {/* <div className="App" style={{
+      backgroundImage: `url(${toolImage})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      width: '100vw',
+      height: '100vh'
+    }} > */}
       {/* <Container>
       <h1 onClick={() => {
         const tools = ['socket', 'wrench', 'screwdriver']
@@ -77,14 +86,22 @@ function App() {
       }}>AutoPit AI</h1>
       </Container> */}
       <Container
-      fluid
-      className="d-flex justify-content-center flex-column align-items-center"
-      style={{ height: '100vh' }}
-    >
-      <h1 style={{ fontSize: '80px'}}>AutoPitAI</h1>
-      <span className='Landing-header' style={{ fontSize: '80px', display: 'block'}}>⬇︎</span>
-      {/* <h1  className='Landing-header' style={{ fontSize: '80px'}}>AutoPitAI</h1> */}
-    </Container>
+        fluid
+        className="d-flex justify-content-center flex-column align-items-center"
+        style={{
+          height: '100vh',
+          backgroundImage: `url(${toolImage})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          width: '100vw',
+        }}
+      >
+        <h1 style={{ fontSize: '80px' }}>AutoPitAI</h1>
+        <span className='Landing-header' style={{ fontSize: '80px', display: 'block', marginTop: '10px' }}>⬇︎</span>
+        <Button style={{ backgroundColor: 'red' }} size='lg'>Fix My Car!</Button>
+        {/* <h1  className='Landing-header' style={{ fontSize: '80px'}}>AutoPitAI</h1> */}
+      </Container>
       {showToolsAndInstructions()}
     </div>
   );
