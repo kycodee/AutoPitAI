053b9f5a9c11c00a78e84baceaa4d3251c69b2c8
kycodee <ky.patton87@gmail.com>
2025-02-11 07:50:13 -0600

    Removed unused files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom"
 import Home  from "./Home"
-import ToolsNeededBox from "./ToolsNeeded";
 import './App.css';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={ <Home /> }/>
-        <Route path="/tools" element={ <ToolsNeededBox /> }/>
       </Routes>
     </div>
     
